9f68b0bcc1ddd8bdb44edf213f8b819bbe41e25c
not-kaz <87723604+not-kaz@users.noreply.github.com>
2025-12-22 21:53:06 +0100

    refactor: simplify window code

--- platform_x11.c
+++ platform_x11.c
@@ -6,16 +6,11 @@
 #include <X11/Xutil.h> /* TODO: Check if this is necessary. */
 #include "platform.h"
 
-#define MAX_NUM_SCREENS 16
-#define MAX_NUM_WINDOWS 32
-
 struct {
 	Display *display;
 	/* NOTE: Only default screen supported for the moment. */
 	int32_t screen_id;
 	Window root_window;
-	Window windows[MAX_NUM_WINDOWS];
-	uint8_t window_count;
 } state = {0};
 
 bool platform_start(void)
@@ -37,30 +32,26 @@ bool platform_start(void)
 
 void platform_shutdown(void)
 {
-	for (uint8_t i = 0; i < state.window_count; i++) {
-		XDestroyWindow(state.display, state.windows[i]);
-	}
 	XCloseDisplay(state.display);
+	/* TODO: Clear 'state' struct. */
 }
 
-struct platform_window platform_create_window(
+void platform_window_init(struct platform_window *window,
 		struct platform_window_desc window_desc)
 {
-	struct platform_window window;
-
 	/* TODO: Figure out how to tackle border and background colors. */
-	state.windows[state.window_count] = XCreateSimpleWindow(state.display,
+	window->handle = (uintptr_t)XCreateSimpleWindow(state.display,
 			state.root_window, window_desc.x, window_desc.y,
 			window_desc.width, window_desc.height, 1,
 			BlackPixel(state.display, state.screen_id), 
 			WhitePixel(state.display, state.screen_id));
-	window.handle = (uintptr_t)state.windows[state.window_count];
-	XMapWindow(state.display, state.windows[state.window_count]);
+	XMapWindow(state.display, window->handle);
 	XFlush(state.display);
-	state.window_count++;
-	return window;
 }
 
-void platform_present_rgba8_buffer(void)
+void platform_window_finish(struct platform_window *window)
+{
+	XDestroyWindow(state.display, window->handle);
+}
 {
 }
