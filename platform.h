21acf665ce4ceb15ab46b93b5c732223973d55f7
not-kaz <87723604+not-kaz@users.noreply.github.com>
2026-01-04 16:31:45 +0100

    style: put func return type on same line

--- platform.h
+++ platform.h
@@ -268,15 +268,17 @@ bool platform_start(void);
 
 void platform_shutdown(void);
 
-struct platform_window *platform_window_create(
-		struct platform_window_desc window_desc);
+struct platform_event platform_poll_event(void);
+
+struct platform_window *platform_window_create(struct platform_window_desc window_desc);
 
 void platform_window_destroy(struct platform_window *window);
 
-struct platform_window_desc platform_window_get_desc(
-		struct platform_window *window);
+struct platform_window_desc platform_window_get_desc(struct platform_window *window);
 
-struct platform_event platform_poll_event(void);
+bool platform_window_should_close(struct platform_window *window);
+
+bool platform_window_is_active(struct platform_window *window);
 
 void platform_window_present_rgba8_buffer(struct platform_window *window,
 		struct platform_rgba8_buffer_desc buffer_desc);
