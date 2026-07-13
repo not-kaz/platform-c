2267b573329d4e80a33d918bccc9aa9d4992f685
not-kaz <kazmiller@disroot.org>
2026-07-13 18:03:33 +0200

    chore: add cc flags

--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,5 @@
 # TEST BUILD
-gcc -g main.c platform_x11.c -lX11 -Wall -Wextra -std=c99 -pedantic
+gcc -g main.c platform_x11.c -lX11 -Wall -Wextra -std=c99 -pedantic \
+	-Wconversion -Wsign-conversion -Wshadow -Wundef -Wformat=2 \
+	-Wstrict-prototypes -Wmissing-prototypes \
+	-Wpointer-arith -Wcast-qual -Wwrite-strings -Wnull-dereference -fanalyzer
